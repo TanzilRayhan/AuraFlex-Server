3158c6651ff2e8591f46868c40e169e0130d6a53
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-11-29 03:00:28 +0600

    Admin verification

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 async function run() {
@@ -30,28 +30,69 @@ async function run() {
     const trainerCollection = client.db("auraFlexDB").collection("trainer");
     const featureCollection = client.db("auraFlexDB").collection("feature");
 
+    // JWT API
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h"
+      });
+      res.send({ token });
+    })
+
+    //middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'forbidden access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
     //user API
-    app.get("/users", async (req, res)=>{
+    app.get("/users", verifyToken, async (req, res) => {
+      console.log(req.headers);
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
-    app.post("/users", async (req, res)=>{
+
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+       admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+    app.post("/users", async (req, res) => {
       const user = req.body;
       //insert email if user doesn't exists
-      const query = {email: user.userEmail}
+      const query = { email: user.email }
       const existingUser = await userCollection.findOne(query);
-      if(existingUser){
-        return res.send({message: 'user already exists', insertedId: null})
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
       }
       const result = await userCollection.insertOne(user);
       console.log(result);
       res.send(result);
     })
 
-    app.patch("/users/admin/:id", async (req, res)=>{
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
           role: 'admin'
@@ -61,29 +102,29 @@ async function run() {
       res.send(result);
     })
 
-    app.delete("/users/:id", async(req, res)=>{
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
 
     //features
-    app.get("/feature", async (req, res)=>{
+    app.get("/feature", async (req, res) => {
       const cursor = featureCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
 
     //trainers
-    app.get("/trainer", async (req, res)=>{
+    app.get("/trainer", async (req, res) => {
       const cursor = trainerCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
 
-    app.post("/trainer", async(req, res)=>{
+    app.post("/trainer", async (req, res) => {
       const trainer = req.body;
       const result = await trainerCollection.insertOne(trainer);
       console.log(result);
